8d7d026c7483bccc909737b2169add786783a57d
JayWinton <Jwinton87@gmail.com>
2016-08-16 20:12:17 -0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {      
-    var isMobile = window.matchMedia("only screen and (max-width: 760px)");
-
-    if (isMobile.matches) {
-        //Conditional script here
-    }
- });
